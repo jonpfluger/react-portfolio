fe9949814c08521010206250edc3e00c59efe108
jonpfluger <jonpfluger718@gmail.com>
2023-01-27 21:39:01 -0600

    submit button styles

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom'
 
 function Navigation() {
     return (
-        <nav id="navbar" className="navbar mx-auto">
-            <Link className="m-2" to="/">About Me</Link>
-            <Link className="m-2" to="/Portfolio">Portfolio</Link>
-            <Link className="m-2" to="/Resume">Resume</Link>
-            <Link className="m-2" to="/Contact">Contact</Link>
+        <nav className="mx-auto">
+            <Link className="navbar-link m-2" to="/">About Me</Link>
+            <Link className="navbar-link m-2" to="/Portfolio">Portfolio</Link>
+            <Link className="navbar-link m-2" to="/Resume">Resume</Link>
+            <Link className="navbar-link m-2" to="/Contact">Contact</Link>
         </nav>
     )
 }
