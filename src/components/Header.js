29216ef801ced18d9678ec55db143edbbe9b2b69
jonpfluger <jonpfluger718@gmail.com>
2023-11-15 09:07:39 -0600

    updated description

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Navigation from './Navigation'
 import logo from '../images/logo3.png'
 
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 function Project(props) {
     const { title, desc, tech, link, repo, imagePath, alt } = props
