e2a36fdaf548ce9d73b9ab31380cd46362e2a61e
jonpfluger <jonpfluger718@gmail.com>
2023-11-15 16:24:02 -0600

    basname is back!

--- src/index.js
+++ src/index.js
@@ -41,6 +41,6 @@ const router = createBrowserRouter([
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={router} basname="https://jpdev.codes" />
   </React.StrictMode>
 );
